bae209456631468e392e26ec4ff21fcf9f4bff5e
rey <3065588496@qq.com>
2022-09-05 13:26:15 +0800

    init

---
+++ aliaes.zsh
@@ -0,0 +1,45 @@
+alias vim="nvim"
+alias vid="neovide"
+alias c="clear"
+alias ra="ranger"
+#alias sproxy1="export http_proxy=http://127.0.0.1:7890"
+#alias sproxy2="export https_proxy=http://127.0.0.1:7890"
+alias proxy="bash ~/scripts/proxy.sh"
+alias cl="bash ~/scripts/clash.sh"
+alias s="cd"
+alias new="mkdir"
+alias ls="exa"
+alias lsa="exa -abghHliS"
+alias sshy="ssh Yongteng@172.16.2.17"
+alias sshr="ssh root@121.40.151.71"
+alias sshp="ssh pi@192.168.0.196"
+
+# go stuff
+alias gi="go mod init"
+alias gt="go mod tidy"
+alias gg="cd;cd go/src"
+alias ga="cd /home/rey/go/src/gruid-rltuto"
+
+# rust stuff
+alias cn="cargo new"
+alias cb="cargo build"
+alias cr="cargo run"
+
+
+# lf stuff
+alias lc="lfcd"
+alias lf="lfub"
+
+# Go Air
+alias gair='~/.air'
+
+# suckless
+alias suck='cd ~/Desktop/suckless'
+
+# rmtrash
+#alias rm='rmtrash'
+#alias rmdir='rmdirtrash'
+alias sudo='sudo '
+
+# k8s
+alias kc='kubectl'
---
+++ env.zsh
@@ -0,0 +1,49 @@
+export EDITOR=/usr/bin/nvim
+export VISUAL=/usr/bin/nvim
+
+#export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+#export LANGUAGE=en_US.UTF-8
+
+#export LC_CTYPE=zh_CN.UTF-8
+#export LC_ALL=zh_CN.UTF-8
+#export LANG=zh_CN.UTF-8
+#export LANGUAGE=zh_CN.UTF-8
+
+# Go
+export GO111MODULE="on"
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/home/rey/.local/bin
+export GOPROXY=https://goproxy.cn,direct
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export PATH=$PATH:$(go env GOPATH)/bin
+
+# Rust
+export PATH=$PATH:$HOME/.cargo/bin
+
+
+# proxy
+export http_proxy=http://127.0.0.1:7890
+export https_proxy=http://127.0.0.1:7890
+
+#script
+export PATH=$PATH:~/scripts/somethingUseful
+
+#$HADOOP_HOME/bin/hadoop fs -cat /usr/root/WordCountV1/input/file01
+
+#$HADOOP_HOME/bin/hadoop fs -put example.txt /usr/root/WordCountV2/input/example
+
+#$HADOOP_HOME/bin/hadoop fs -mkdir /usr/root/WordCountV2/input/example
+
+#$HADOOP_HOME/bin/hadoop jar wc.jar WordCount /usr/root/WordCountV1/input /usr/root/WordCountV1/output
+
+#$HADOOP_HOME/bin/hadoop fs -rmdir -f  /usr/root/WordCountV1/output/
+
+#$HADOOP_HOME/bin/hadoop fs -cat  /usr/root/WordCountV1/output/part-r-00000
+#$HADOOP_HOME/bin/hadoop fs -cat  /usr/root/WordCountV2/output/part-r-00000
+
+#$HADOOP_HOME/bin/hadoop jar wc.jar WordCount2 /usr/root/WordCountV2/input/example /usr/root/WordCountV2/output
+
+#$HADOOP_HOME/bin/hadoop fs -rm -r  /usr/root/WordCountV2/input/
+#$HADOOP_HOME/bin/hadoop fs -rm -r  /usr/root/WordCountV2/output/
